462dac97b7f00e64b93ec9bac96e44ad23fa7ddc
Mykola Myhailov <mykola.myhailov@gmail.com>
2017-12-19 18:04:48 +0200

    [GLOBAl] - ApiInterface

--- app/src/main/java/com/myhailov/mykola/fishpay/activities/drawer/ContactsActivity.java
+++ app/src/main/java/com/myhailov/mykola/fishpay/activities/drawer/ContactsActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import com.myhailov.mykola.fishpay.R;
 import com.myhailov.mykola.fishpay.activities.DrawerActivity;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class ContactsActivity extends DrawerActivity {
 
     @Override
@@ -16,10 +19,16 @@ public class ContactsActivity extends DrawerActivity {
 
         initToolbar(getString(R.string.my_contacts));
         createDrawer();
+
     }
 
     @Override
     public void onClick(View view) {
 
     }
+
+
+
+
+
 }
