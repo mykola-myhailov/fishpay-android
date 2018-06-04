cd8cd2b84a987e54147a70811d1b19acc902b6a5
Demandr <ol.demandr13108@gmail.com>
2018-06-04 14:36:56 +0300

    Fix change password

--- app/src/main/java/com/myhailov/mykola/fishpay/activities/profile/ChangePasswordActivity.java
+++ app/src/main/java/com/myhailov/mykola/fishpay/activities/profile/ChangePasswordActivity.java
@@ -2,52 +2,94 @@ package com.myhailov.mykola.fishpay.activities.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.myhailov.mykola.fishpay.R;
 import com.myhailov.mykola.fishpay.activities.BaseActivity;
 import com.myhailov.mykola.fishpay.api.ApiClient;
 import com.myhailov.mykola.fishpay.api.BaseCallback;
+import com.myhailov.mykola.fishpay.api.BaseResponse;
 import com.myhailov.mykola.fishpay.api.results.ChangePassVerifyResult;
 import com.myhailov.mykola.fishpay.utils.Keys;
 import com.myhailov.mykola.fishpay.utils.TokenStorage;
 import com.myhailov.mykola.fishpay.utils.Utils;
 
+import retrofit2.Call;
+
 public class ChangePasswordActivity extends BaseActivity {
 
     private EditText etPassword;
+    private TextView tvPasswordIncorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
-        initToolBar("Изменить пароль");
+        initCustomToolbar("Изменить пароль");
 
+        tvPasswordIncorrect = findViewById(R.id.tv_password_incorrect);
         findViewById(R.id.tvNext).setOnClickListener(this);
         findViewById(R.id.ivNextArrow).setOnClickListener(this);
         etPassword = findViewById(R.id.etPassword);
+        etPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    checkPassword();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ivBack:
+                onBackPressed();
+                break;
+            case R.id.tvNext:
+                checkPassword();
+                break;
+            case R.id.ivNextArrow:
+                checkPassword();
+                break;
+        }
+
+    }
+
+    private void checkPassword() {
         String password = etPassword.getText().toString();
         if (password.equals("")) Utils.toast(context, getString(R.string.enter_password));
-        else if (password.length() < 8) Utils.toast(context, getString(R.string.short_password));
+        else if (password.length() < 8)
+            Utils.toast(context, getString(R.string.short_password));
         else if (!Utils.isOnline(context)) Utils.noInternetToast(context);
-        else changePassVerifyRequest(password);
+        else {
+            changePassVerifyRequest(password);
+        }
     }
 
     /*  title = "изменить пароль"  */
-    private void changePassVerifyRequest(String password){
+    private void changePassVerifyRequest(String password) {
         String token = TokenStorage.getToken(context);
         ApiClient.getApiClient().changePassVerify(token, password)
                 .enqueue(new BaseCallback<ChangePassVerifyResult>(context, true) {
                     @Override
+                    public void onFailure(@NonNull Call<BaseResponse<ChangePassVerifyResult>> call, @NonNull Throwable t) {
+                        tvPasswordIncorrect.setVisibility(View.VISIBLE);
+                        super.onFailure(call, t);
+                    }
+
+                    @Override
                     protected void onResult(int code, ChangePassVerifyResult result) {
-                        if (code == 200){
+                        if (code == 200) {
+                            tvPasswordIncorrect.setVisibility(View.GONE);
                             Intent intent = new Intent(context, NewPasswordActivity.class);
-                            intent.putExtra(Keys.PASS_CHANGE_ID,  result.getPassChangeId());
+                            intent.putExtra(Keys.PASS_CHANGE_ID, result.getPassChangeId());
                             context.startActivity(intent);
                         }
                     }
--- app/src/main/java/com/myhailov/mykola/fishpay/activities/profile/NewPasswordActivity.java
+++ app/src/main/java/com/myhailov/mykola/fishpay/activities/profile/NewPasswordActivity.java
@@ -2,12 +2,19 @@ package com.myhailov.mykola.fishpay.activities.profile;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.myhailov.mykola.fishpay.R;
 import com.myhailov.mykola.fishpay.activities.BaseActivity;
@@ -22,6 +29,8 @@ public class NewPasswordActivity extends BaseActivity {
 
     private String passChangeId;
     private EditText etPassword;
+    private TextView tvOk;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +41,33 @@ public class NewPasswordActivity extends BaseActivity {
         findViewById(R.id.tvNext).setOnClickListener(this);
         findViewById(R.id.ivNextArrow).setOnClickListener(this);
         etPassword = findViewById(R.id.etPassword);
+        etPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    checkPassword();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tvNext:
+                checkPassword();
+                break;
+            case R.id.ivNextArrow:
+                checkPassword();
+                break;
+            case R.id.tv_action_1:
+                alertDialog.cancel();
+                startActivity(new Intent(context, BeginActivity.class));
+                break;
+        }
+    }
 
+    private void checkPassword(){
         String password = etPassword.getText().toString();
         if (password.equals("")) Utils.toast(context, getString(R.string.enter_password));
         else if (password.length() < 8) Utils.toast(context, getString(R.string.short_password));
@@ -53,17 +84,29 @@ public class NewPasswordActivity extends BaseActivity {
                 new BaseCallback<Object>(context, true) {
                     @Override
                     protected void onResult(int code, Object result) {
-                        new AlertDialog.Builder(context)
-                                .setMessage("Пароль успешно изменён. Для входа в систему воспользуйтесь новым паролем")
-                                .setPositiveButton(context.getString(R.string.ok), new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        context.startActivity(new Intent(context, BeginActivity.class));
-                                    }
-                                })
-                                .create().show();
+                        showAlert();
                     }
                 }
         );
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_with_one_action, null);
+        dialogBuilder.setView(dialogView);
+        tvOk = dialogView.findViewById(R.id.tv_action_1);
+
+        tvOk.setOnClickListener(this);
+
+        alertDialog = dialogBuilder.create();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                startActivity(new Intent(context, BeginActivity.class));
+            }
+        });
+        alertDialog.show();
     }
 }
