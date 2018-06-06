ee05f4c3be41e003bc4576fa8fded7473937451c
Demandr <ol.demandr13108@gmail.com>
2018-06-06 14:38:38 +0300

    Fix ui

--- app/src/main/java/com/myhailov/mykola/fishpay/api/results/CharityResultById.java
+++ app/src/main/java/com/myhailov/mykola/fishpay/api/results/CharityResultById.java
@@ -5,37 +5,172 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 import java.util.List;
 
-public class CharityResultById implements Serializable{
-        @SerializedName("id")
+public class CharityResultById implements Serializable {
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("description")
+    private String description;
+    @SerializedName("required_amount")
+    private Integer requiredAmount;
+    @SerializedName("init_collected_amount")
+    private Integer initCollectedAmount;
+    @SerializedName("main_photo")
+    private String mainPhoto;
+    @SerializedName("author_id")
+    private Integer authorId;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("status")
+    private String status;
+    @SerializedName("created_at")
+    private String createdAt;
+    @SerializedName("author_name")
+    private String authorName;
+    @SerializedName("total_amount")
+    private Integer totalAmount;
+    @SerializedName("execution")
+    private Double execution;
+    @SerializedName("photos")
+    private List<Photo> photos = null;
+    @SerializedName("donation")
+    private List<Donation> donation = null;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRequiredAmount() {
+        return requiredAmount;
+    }
+
+    public void setRequiredAmount(Integer requiredAmount) {
+        this.requiredAmount = requiredAmount;
+    }
+
+    public Integer getInitCollectedAmount() {
+        return initCollectedAmount;
+    }
+
+    public void setInitCollectedAmount(Integer initCollectedAmount) {
+        this.initCollectedAmount = initCollectedAmount;
+    }
+
+    public String getMainPhoto() {
+        return mainPhoto;
+    }
+
+    public void setMainPhoto(String mainPhoto) {
+        this.mainPhoto = mainPhoto;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+
+    public Double getExecution() {
+        return execution;
+    }
+
+    public void setExecution(Double execution) {
+        this.execution = execution;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public List<Donation> getDonation() {
+        return donation;
+    }
+
+    public void setDonation(List<Donation> donation) {
+        this.donation = donation;
+    }
+
+    public class Photo implements Serializable {
+        @SerializedName("charity_item_id")
         private Integer id;
-        @SerializedName("title")
-        private String title;
-        @SerializedName("description")
-        private String description;
-        @SerializedName("required_amount")
-        private Integer requiredAmount;
-        @SerializedName("init_collected_amount")
-        private Integer initCollectedAmount;
-        @SerializedName("main_photo")
-        private String mainPhoto;
-        @SerializedName("author_id")
-        private Integer authorId;
-        @SerializedName("name")
-        private Object name;
-        @SerializedName("status")
-        private String status;
+        @SerializedName("photo_url")
+        private String photoUrl;
+        @SerializedName("order")
+        private Integer order;
         @SerializedName("created_at")
         private String createdAt;
-        @SerializedName("author_name")
-        private String authorName;
-        @SerializedName("total_amount")
-        private Integer totalAmount;
-        @SerializedName("execution")
-        private Double execution;
-        @SerializedName("photos")
-        private List<Photo> photos = null;
-        @SerializedName("donation")
-        private List<Donation> donation = null;
+        @SerializedName("updated_at")
+        private String updatedAt;
 
         public Integer getId() {
             return id;
@@ -45,70 +180,21 @@ public class CharityResultById implements Serializable{
             this.id = id;
         }
 
-        public String getTitle() {
-            return title;
+        public String getPhotoUrl() {
+            return photoUrl;
         }
 
-        public void setTitle(String title) {
-            this.title = title;
+        public void setPhotoUrl(String photoUrl) {
+            this.photoUrl = photoUrl;
         }
 
-        public String getDescription() {
-            return description;
+        public Integer getOrder() {
+            return order;
         }
 
-        public void setDescription(String description) {
-            this.description = description;
+        public void setOrder(Integer order) {
+            this.order = order;
         }
-
-        public Integer getRequiredAmount() {
-            return requiredAmount;
-        }
-
-        public void setRequiredAmount(Integer requiredAmount) {
-            this.requiredAmount = requiredAmount;
-        }
-
-        public Integer getInitCollectedAmount() {
-            return initCollectedAmount;
-        }
-
-        public void setInitCollectedAmount(Integer initCollectedAmount) {
-            this.initCollectedAmount = initCollectedAmount;
-        }
-
-        public String getMainPhoto() {
-            return mainPhoto;
-        }
-
-        public void setMainPhoto(String mainPhoto) {
-            this.mainPhoto = mainPhoto;
-        }
-
-        public Integer getAuthorId() {
-            return authorId;
-        }
-
-        public void setAuthorId(Integer authorId) {
-            this.authorId = authorId;
-        }
-
-        public Object getName() {
-            return name;
-        }
-
-        public void setName(Object name) {
-            this.name = name;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
 
         public String getCreatedAt() {
             return createdAt;
@@ -118,100 +204,16 @@ public class CharityResultById implements Serializable{
             this.createdAt = createdAt;
         }
 
-        public String getAuthorName() {
-            return authorName;
+        public String getUpdatedAt() {
+            return updatedAt;
         }
 
-        public void setAuthorName(String authorName) {
-            this.authorName = authorName;
+        public void setUpdatedAt(String updatedAt) {
+            this.updatedAt = updatedAt;
         }
-        public Integer getTotalAmount() {
-            return totalAmount;
-        }
+    }
 
-        public void setTotalAmount(Integer totalAmount) {
-            this.totalAmount = totalAmount;
-        }
-
-
-        public Double getExecution() {
-            return execution;
-        }
-
-        public void setExecution(Double execution) {
-            this.execution = execution;
-        }
-
-        public List<Photo> getPhotos() {
-            return photos;
-        }
-
-        public void setPhotos(List<Photo> photos) {
-            this.photos = photos;
-        }
-
-        public List<Donation> getDonation() {
-            return donation;
-        }
-
-        public void setDonation(List<Donation> donation) {
-            this.donation = donation;
-        }
-
-        public class Photo implements Serializable{
-            @SerializedName("charity_item_id")
-            private Integer id;
-            @SerializedName("photo_url")
-            private String photoUrl;
-            @SerializedName("order")
-            private Integer order;
-            @SerializedName("created_at")
-            private String createdAt;
-            @SerializedName("updated_at")
-            private String updatedAt;
-
-            public Integer getId() {
-                return id;
-            }
-
-            public void setId(Integer id) {
-                this.id = id;
-            }
-
-            public String getPhotoUrl() {
-                return photoUrl;
-            }
-
-            public void setPhotoUrl(String photoUrl) {
-                this.photoUrl = photoUrl;
-            }
-
-            public Integer getOrder() {
-                return order;
-            }
-
-            public void setOrder(Integer order) {
-                this.order = order;
-            }
-
-            public String getCreatedAt() {
-                return createdAt;
-            }
-
-            public void setCreatedAt(String createdAt) {
-                this.createdAt = createdAt;
-            }
-
-            public String getUpdatedAt() {
-                return updatedAt;
-            }
-
-            public void setUpdatedAt(String updatedAt) {
-                this.updatedAt = updatedAt;
-            }
-        }
-
-    public class Donation implements Serializable{
+    public class Donation implements Serializable {
 
         @SerializedName("id")
         private Integer id;
@@ -285,8 +287,6 @@ public class CharityResultById implements Serializable{
         }
 
     }
-
-
 
 
 }
